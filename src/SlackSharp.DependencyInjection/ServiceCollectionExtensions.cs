0bcefd545de65d71e85c1540232f762edc51d0ff
t.takahari <taiga1018@gmail.com>
2019-03-08 11:48:48 +0900

    add DI implementation.

---
+++ src/SlackSharp.DependencyInjection/ServiceCollectionExtensions.cs
@@ -0,0 +1,47 @@
+﻿using JsonHttpContentConverter;
+using SlackSharp;
+using System;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    /// <summary>
+    /// Extensions of <see cref="IServiceCollection"/>.
+    /// </summary>
+    public static class ServiceCollectionExtensions
+    {
+        /// <summary>
+        /// <para>Add <see cref="IWebHookClient"/> to resolve an instance with DI.</para>
+        /// <para>If you use this method, you have to add an instance of <see cref="IJsonHttpContentConverter"/> to <see cref="IServiceCollection"/>.</para>
+        /// </summary>
+        /// <param name="services"><see cref="IServiceCollection"/> to add services to.</param>
+        /// <returns>Added <see cref="IServiceCollection"/>.</returns>
+        public static IServiceCollection AddSlackWebHookClient(this IServiceCollection services)
+        {
+            if (services == null)
+            {
+                throw new ArgumentNullException(nameof(services));
+            }
+
+            services.AddTransient<IWebHookClient>(service => new WebHookClient(service.GetService<IJsonHttpContentConverter>()));
+
+            return services;
+        }
+
+        /// <summary>
+        /// Add <see cref="IWebHookClient"/> with an instance of <see cref="IJsonHttpContentConverter"/> to resolve an instance with DI.
+        /// </summary>
+        /// <param name="services"><see cref="IServiceCollection"/> to add services to.</param>
+        /// <param name="converter">An instance of <see cref="IJsonHttpContentConverter"/>.</param>
+        /// <returns>Added <see cref="IServiceCollection"/>.</returns>
+        public static IServiceCollection AddSlackWebHookClient(this IServiceCollection services, IJsonHttpContentConverter converter)
+        {
+            if (services == null) throw new ArgumentNullException(nameof(services));
+            if (converter == null) throw new ArgumentNullException(nameof(converter));
+
+            services.AddSingleton(converter);
+            services.AddTransient<IWebHookClient>(service => new WebHookClient(service.GetService<IJsonHttpContentConverter>()));
+
+            return services;
+        }
+    }
+}
